b4d3f6d71adee2e5e684f475ff13a9abcfc5d9b4
Lockett Pundt <kalifornium@hotmail.com>
2020-03-31 12:44:48 -0400

    deleted unused variables and console.logs. added a div around the labels with className labelbox to put them in flex row.

--- src/components/IssueDetail.jsx
+++ src/components/IssueDetail.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import {Icon, Container, Section} from 'bloomer';
+import { Icon, Section } from 'bloomer';
 import Markdown from 'react-markdown/with-html';
 
 class IssueDetail extends Component {
@@ -11,56 +14,66 @@ class IssueDetail extends Component {
       userName: '',
       userLink: '#',
       body: '',
-      status: '',
     };
-  };
-  
+  }
+
+
+  async componentDidMount() {
+    const { issue_number } = this.props.match.params;
+    const issueData = await this.fetchIssue(issue_number);
+
+    this.setState({
+      issueData,
+      userImage: issueData.user.avatar_url,
+      body: issueData.body,
+      userLink: issueData.user.html_url,
+      userName: issueData.user.login,
+    });
+  }
+
   fetchIssue = async (num) => {
     const response = await fetch(`https://api.github.com/repos/facebook/create-react-app/issues/${num}`);
     const json = await response.json();
     return json;
   }
-  
-  async componentDidMount() {
-    const num = this.props.match.params.issue_number;
-    const issueData = await this.fetchIssue(num);
-    
-    this.setState({
-      issueData,
-      userImage: issueData.user.avatar_url,
-      body: issueData.body,
-      status: issueData.status,
-      userLink: issueData.user.html_url,
-      userName: issueData.user.login,
-    });
-  }
-  
-  
+
 
   render() {
-    
-    const { issueData, userImage, userName, userLink, body, status } = this.state;
-    const statusIcon = <Icon isSize="small" className="fas fa-exclamation-circle"/>
-    const labelInfo = !!issueData.labels ? issueData.labels.map( item => {
+    const {
+      issueData, userImage, userName, userLink, body,
+    } = this.state;
+    const statusIcon = <Icon isSize="small" className="fas fa-exclamation-circle" />;
+    const labelInfo = issueData.labels ? issueData.labels.map((item) => {
       const color = {
         backgroundColor: `#${item.color}`,
         color: 'white',
-      }
-      return <div className="label" style={color}>{item.name}</div>})
-       : '';
-       
+      };
+      return <div className="label" style={color}>{item.name}</div>;
+    })
+      : '';
+
     return (
       <Section className="issueBox">
         {statusIcon}
         <a href={issueData.html_url} target="" rel="noopener noreferrer">{issueData.title}</a>
         <div className="labelBox">
-        {labelInfo}
+          {labelInfo}
         </div>
-        <p>#{issueData.number} Opened by <a href={userLink} rel="noopener noreferrer">{userName}</a> <img src={userImage} alt="user prifile pic"/> </p>
+        <p>
+          #
+          {issueData.number}
+          {' '}
+          Opened by
+          {' '}
+          <a href={userLink} rel="noopener noreferrer">{userName}</a>
+          {' '}
+          <img src={userImage} alt="user prifile pic" />
+          {' '}
+        </p>
         <Markdown className="bodyText" source={body} escapeHtml={false} />
-        <a href='/' rel="noopener noreferrer">Back</a>
+        <a href="/" rel="noopener noreferrer">Back</a>
       </Section>
-      
+
     );
   }
 }
