bda37a1a143453ab380e8a8655e5081774ae02fd
Lockett Pundt <kalifornium@hotmail.com>
2020-03-31 12:03:16 -0400

    imported the router and created two routes. One for the index, and the other route is for individual issues that take a url paramater of the issue number.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import IssueList from './components/IssueList';
 import '../node_modules/bulma/css/bulma.css';
+import IssueDetail from './components/IssueDetail';
+
 
 function App() {
   return (
     <div className="App">
-      <IssueList />
+      <Router>
+        <Route path='/' exact component={IssueList}/>
+        <Route path='/issues/:issue_number?' exact component={IssueDetail}/>
+      </Router>
     </div>
   );
 }
