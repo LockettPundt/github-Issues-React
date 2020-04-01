aef45788179cafb6bf7f4fc5193d487e8b961119
Lockett Pundt <kalifornium@hotmail.com>
2020-04-01 13:32:43 -0400

    made this a functional component using hooks for the state and a hook to set the state with an API call.

--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,68 +1,63 @@
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Section, Icon } from 'bloomer';
+import axios from 'axios';
 
-class IssueList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      issueInfo: [],
+const IssueList = () => {
+  const [issueInfo, setIssueInfo] = useState([]);
+
+
+  useEffect(() => {
+    const response = async () => {
+      const result = await axios.get('https://api.github.com/repos/facebook/create-react-app/issues');
+      setIssueInfo(result.data);
     };
-  }
-
-  componentDidMount = async () => {
-    const response = await fetch('https://api.github.com/repos/facebook/create-react-app/issues');
-    const json = await response.json();
-    this.setState({
-      issueInfo: json,
-    });
-  }
-
-  render() {
-    const { issueInfo } = this.state;
-    const issues = issueInfo ? issueInfo.map((item) => {
-      const statusIcon = <Icon isSize="small" className="fas fa-exclamation-circle" />;
-      const labelInfo = item.labels ? item.labels.map((label) => {
-        const color = {
-          backgroundColor: `#${label.color}`,
-          color: 'white',
-        };
-        return <div className="label" style={color}>{label.name}</div>;
-      })
-        : '';
-
-      const link = `/issues/${item.number}`;
-      return (
-        <Section className="issueBox">
-          {statusIcon}
-          <a href={link}>{item.title}</a>
-          <div className="labelBox">
-            {labelInfo}
-          </div>
-          <p>
-            #
-            {item.number}
-            {' '}
-            Opened by
-            <a href={item.user.html_url} rel="noopener noreferrer">
-              {' '}
-              {item.user.login}
-            </a>
-            {' '}
-            <img src={item.user.avatar_url} alt="user prifile pic" />
-            {' '}
-          </p>
-        </Section>
-      );
-    }) : '';
+    response();
+  }, []);
 
 
+  const issues = issueInfo ? issueInfo.map((item) => {
+    const statusIcon = <Icon isSize="small" className="fas fa-exclamation-circle" />;
+    const labelInfo = item.labels ? item.labels.map((label) => {
+      const color = {
+        backgroundColor: `#${label.color}`,
+        color: 'white',
+      };
+      return <div className="label" style={color}>{label.name}</div>;
+    })
+      : '';
+
+    const link = `/issues/${item.number}`;
     return (
-      <Section className="issueList">
-        {issues}
+      <Section className="issueBox">
+        {statusIcon}
+        <a href={link}>{item.title}</a>
+        <div className="labelBox">
+          {labelInfo}
+        </div>
+        <p>
+          #
+          {item.number}
+          {' '}
+          Opened by
+          <a href={item.user.html_url} rel="noopener noreferrer">
+            {' '}
+            {item.user.login}
+          </a>
+          {' '}
+          <img src={item.user.avatar_url} alt="user prifile pic" />
+          {' '}
+        </p>
       </Section>
     );
-  }
-}
+  }) : '';
+
+
+  return (
+    <Section className="issueList">
+      {issues}
+    </Section>
+  );
+};
 
 export default IssueList;
