93e76439815acc9c65704f03e1428f4918e82232
Lockett Pundt <kalifornium@hotmail.com>
2020-04-01 13:33:58 -0400

    changed from a class to a functional component using hooks for state management.

--- src/components/IssueDetail.jsx
+++ src/components/IssueDetail.jsx
@@ -1,82 +1,61 @@
 /* eslint-disable camelcase */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon, Section } from 'bloomer';
 import Markdown from 'react-markdown/with-html';
+import axios from 'axios';
 
-class IssueDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      issueData: [],
-      userImage: '',
-      userName: '',
-      userLink: '#',
-      body: '',
+
+const IssueDetail = (props) => {
+  const [issueData, setIssueData] = useState([]);
+
+
+  useEffect(() => {
+    const { issue_number } = props.match.params;
+    const response = async () => {
+      const result = await axios.get(`https://api.github.com/repos/facebook/create-react-app/issues/${issue_number}`);
+      setIssueData(result.data);
     };
-  }
+    response();
+  }, []);
 
+  const statusIcon = <Icon isSize="small" className="fas fa-exclamation-circle" />;
+  const labelInfo = issueData.labels ? issueData.labels.map((item) => {
+    const color = {
+      backgroundColor: `#${item.color}`,
+      color: 'white',
+    };
+    return <div className="label" style={color}>{item.name}</div>;
+  })
+    : '';
+  const userInfo = issueData.user
+    ? (
+      <div>
+        <a href={issueData.user.html_url}>{issueData.user.login}</a>
+        <img src={issueData.user.avatar_url} alt="user profile pic" />
+      </div>
+    ) : '';
+  return (
+    <Section className="issueBox">
+      {statusIcon}
+      <a href={issueData.html_url} target="" rel="noopener noreferrer">{issueData.title}</a>
+      <div className="labelBox">
+        {labelInfo}
+      </div>
+      <div>
+        #
+        {issueData.number}
+        {' '}
+        Opened by
+        {userInfo}
+      </div>
+      <Markdown className="bodyText" source={issueData.body} escapeHtml={false} />
+      <a href="/" rel="noopener noreferrer">Back</a>
+    </Section>
 
-  async componentDidMount() {
-    const { issue_number } = this.props.match.params;
-    const issueData = await this.fetchIssue(issue_number);
-
-    this.setState({
-      issueData,
-      userImage: issueData.user.avatar_url,
-      body: issueData.body,
-      userLink: issueData.user.html_url,
-      userName: issueData.user.login,
-    });
-  }
-
-  fetchIssue = async (num) => {
-    const response = await fetch(`https://api.github.com/repos/facebook/create-react-app/issues/${num}`);
-    const json = await response.json();
-    return json;
-  }
-
-
-  render() {
-    const {
-      issueData, userImage, userName, userLink, body,
-    } = this.state;
-    const statusIcon = <Icon isSize="small" className="fas fa-exclamation-circle" />;
-    const labelInfo = issueData.labels ? issueData.labels.map((item) => {
-      const color = {
-        backgroundColor: `#${item.color}`,
-        color: 'white',
-      };
-      return <div className="label" style={color}>{item.name}</div>;
-    })
-      : '';
-
-    return (
-      <Section className="issueBox">
-        {statusIcon}
-        <a href={issueData.html_url} target="" rel="noopener noreferrer">{issueData.title}</a>
-        <div className="labelBox">
-          {labelInfo}
-        </div>
-        <p>
-          #
-          {issueData.number}
-          {' '}
-          Opened by
-          {' '}
-          <a href={userLink} rel="noopener noreferrer">{userName}</a>
-          {' '}
-          <img src={userImage} alt="user prifile pic" />
-          {' '}
-        </p>
-        <Markdown className="bodyText" source={body} escapeHtml={false} />
-        <a href="/" rel="noopener noreferrer">Back</a>
-      </Section>
-
-    );
-  }
-}
+  );
+};
 
 
 export default IssueDetail;
